94d40ad4c3c0a7b7d4f58f6fa6d2f33caf420cfd
Tim Mocking <39952912+timmocking@users.noreply.github.com>
2024-07-30 13:05:54 +0200

    Update all plots

--- plots.py
+++ plots.py
@@ -7,8 +7,9 @@ from sklearn import neighbors
 from sklearn.cluster import KMeans
 from sklearn.metrics import roc_curve, auc
 from scipy.spatial import cKDTree
-
+from matplotlib.colors import LinearSegmentedColormap
 from simulate import simulate_samples
+from scipy.spatial import ConvexHull
 
 def plot_simulation(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukemic_AML3,
                     mean_healthy_x, std_healthy_x, mean_healthy_y, std_healthy_y,
@@ -21,7 +22,8 @@ def plot_simulation(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukem
         label_diseased = "Leukemic"
         label_x = "LAIP1"
         label_y = "LAIP2"
-        sns.scatterplot(data=data, x=label_x, y=label_y, hue="Cluster", hue_order=[label_healthy, label_diseased], ax=ax)
+        sns.scatterplot(data=data, x=label_x, y=label_y, hue="Cluster", hue_order=[label_healthy, label_diseased],
+        s=4, palette=["#009292ff", "#ff7f00ff"], ax=ax, legend=None, rasterized=True)
         ax.set_xlim(xlims)
         ax.set_ylim(ylims)
         ax.set_title(title)
@@ -31,10 +33,10 @@ def plot_simulation(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukem
                                              dist_healthy_LAIP1, std_LAIP1_x, std_LAIP1_y,
                                              dist_healthy_LAIP2, std_LAIP2_x, std_LAIP2_y)
 
-    xlims = (-6, 6)
-    ylims = (-6, 6)
-    
-    fig = plt.figure(figsize=(20, 5))
+    xlims = (-3, 4)
+    ylims = (-3, 4)
+
+    fig = plt.figure(figsize=(7, 2))
     gs = gridspec.GridSpec(1, 4, figure=fig)
     
     # Subplots
@@ -51,7 +53,7 @@ def plot_simulation(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukem
     plot_subplot(AML3, "AML3", ax4)
 
     plt.tight_layout()
-    plt.show()
+    plt.close(fig)
     return fig
 
 
@@ -74,32 +76,25 @@ def plot_supervised(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukem
 
         Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
         Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z, alpha=0.3, cmap=plt.cm.coolwarm)
-        sns.scatterplot(data=data, x=label_x, y=label_y, hue="Cluster", hue_order=[label_healthy, label_diseased], ax=ax)
+        cmap = LinearSegmentedColormap.from_list("custom_cmap", ["#009292ff", "#ff7f00ff"])
+
+        ax.contourf(xx, yy, Z, alpha=0.2, cmap=cmap)
+        data["Prediction"] = clf.predict(data[["LAIP1", "LAIP2"]]) 
+        sns.scatterplot(data=data, x=label_x, y=label_y, s=4, hue="Cluster", hue_order=[label_healthy, label_diseased],
+        palette=["#009292ff", "#ff7f00ff"], ax=ax, rasterized=True, legend=None)
         ax.set_xlim(xlims)
         ax.set_ylim(ylims)
         ax.set_title(title)
-
-    def plot_roc_curve(ax, clf, data, label):
-        # To-do: refractor
-        label_healthy = "Non-leukemic"
-        label_diseased = "Leukemic"
-        label_x = "LAIP1"
-        label_y = "LAIP2"
-
-        y_score = clf.predict_proba(data[[label_x, label_y]])[:, 1]
-        fpr, tpr, _ = roc_curve(np.where(data["Cluster"] == label_diseased, 1, 0), y_score)
-        roc_auc = auc(fpr, tpr)
-        ax.plot(fpr, tpr, label=f'{label} (AUC = {roc_auc:.2f})')
 
     NBM, AML1, AML2, AML3 = simulate_samples(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukemic_AML3,
                                              mean_healthy_x, std_healthy_x, mean_healthy_y, std_healthy_y,
                                              dist_healthy_LAIP1, std_LAIP1_x, std_LAIP1_y,
                                              dist_healthy_LAIP2, std_LAIP2_x, std_LAIP2_y)
 
-    xlims = (-6, 6)
-    ylims = (-6, 6)
-    fig = plt.figure(figsize=(20, 5))
+    xlims = (-3, 4)
+    ylims = (-3, 4)
+
+    fig = plt.figure(figsize=(5.25, 2))
     gs = gridspec.GridSpec(1, 3, figure=fig)
     
     # Train nearest-neighbor classifier on AML1
@@ -123,9 +118,8 @@ def plot_supervised(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukem
     plot_decision_boundary(ax3, clf, xx, yy, AML3, "AML3")
 
     plt.tight_layout()
-    plt.show()
+    plt.close(fig)
     return fig
-
 
 
 def cluster_with_normal(control_data, test_data, k=6):
@@ -167,61 +161,54 @@ def plot_cluster_with_normal(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, fr
         label_diseased = "Leukemic"
         label_x = "LAIP1"
         label_y = "LAIP2"
+
+        for cluster in data["KMeans"].unique():
+            enrichment = list(data[data["KMeans"]==cluster]["Enrichment"])[0]
+            if enrichment > 0.95:
+                fill = "#ff7f00ff"
+            else:
+                fill = "#009292ff"
+
+            points = np.array(data[data["KMeans"]==cluster][["LAIP1", "LAIP2"]])
+            hull = ConvexHull(points)
+            vert = np.append(hull.vertices, hull.vertices[0])
+            ax.plot(points[vert, 0], points[vert, 1], '--', c="black", linewidth=1)
+            ax.fill(points[vert, 0], points[vert, 1], c=fill, alpha=0.2)
+
         data["Prediction"] = np.where(data["Enrichment"] > 0.95, label_diseased, label_healthy)
-        sns.scatterplot(data=data, x="LAIP1", y="LAIP2", hue="KMeans", style="Prediction", palette="tab10", legend=None, ax=ax)        
+        sns.scatterplot(data=data, x=label_x, y=label_y, s=4, hue="Cluster", hue_order=[label_healthy, label_diseased],
+        palette=["#009292ff", "#ff7f00ff"], ax=ax, rasterized=True, legend=None)      
         ax.set_xlim(xlims)
         ax.set_ylim(ylims)
         ax.set_title(title)
-
-    def plot_barplot(data, ax):
-        table = data[["KMeans", "Enrichment"]].value_counts().reset_index().sort_values("KMeans")
-        sns.barplot(data=table, x="KMeans", y="Enrichment", palette="tab10", ax=ax)
-        ax.set_ylim(0, 1.05)
-
-    def plot_roc_curve(ax, data, label):
-        # To-do: refractor
-        label_healthy = "Non-leukemic"
-        label_diseased = "Leukemic"
-        label_x = "LAIP1"
-        label_y = "LAIP2"
-        fpr, tpr, _ = roc_curve(np.where(data["Cluster"] == label_diseased, 1, 0), data["Enrichment"])
-        roc_auc = auc(fpr, tpr)
-        ax.plot(fpr, tpr, label=f'{label} (AUC = {roc_auc:.2f})')
 
     NBM, AML1, AML2, AML3 = simulate_samples(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukemic_AML3,
                                              mean_healthy_x, std_healthy_x, mean_healthy_y, std_healthy_y,
                                              dist_healthy_LAIP1, std_LAIP1_x, std_LAIP1_y,
                                              dist_healthy_LAIP2, std_LAIP2_x, std_LAIP2_y)
 
+    xlims = (-3, 4)
+    ylims = (-3, 4)
 
-    xlims = (-6, 6)
-    ylims = (-6, 6)
-    
     # Get cluster-with-normal results
     AML1["KMeans"], AML1["Enrichment"] = cluster_with_normal(NBM, AML1)
     AML2["KMeans"], AML2["Enrichment"] = cluster_with_normal(NBM, AML2)
     AML3["KMeans"], AML3["Enrichment"] = cluster_with_normal(NBM, AML3)
 
-    fig = plt.figure(figsize=(20, 5))
-    gs = gridspec.GridSpec(2, 3, figure=fig, height_ratios=[0.7, 0.3])
+    fig = plt.figure(figsize=(5.25, 2))
+    gs = gridspec.GridSpec(1, 3, figure=fig)
 
-    ax1a = fig.add_subplot(gs[0, 0])
-    plot_scatter(AML1, "AML1", ax1a)
-    ax1b = fig.add_subplot(gs[1, 0])
-    plot_barplot(AML1, ax1b)
+    ax1 = fig.add_subplot(gs[0, 0])
+    plot_scatter(AML1, "AML1", ax1)
 
-    ax2a = fig.add_subplot(gs[0, 1])
-    plot_scatter(AML2, "AML2", ax2a)
-    ax2b = fig.add_subplot(gs[1, 1])
-    plot_barplot(AML2, ax2b)
+    ax2 = fig.add_subplot(gs[0, 1])
+    plot_scatter(AML2, "AML2", ax2)
 
-    ax3a = fig.add_subplot(gs[0, 2])
-    plot_scatter(AML3, "AML3", ax3a)
-    ax3b = fig.add_subplot(gs[1, 2])
-    plot_barplot(AML3, ax3b)
+    ax3 = fig.add_subplot(gs[0, 2])
+    plot_scatter(AML3, "AML3", ax3)
 
     plt.tight_layout()
-    plt.show()
+    plt.close(fig)
     return fig
 
 
@@ -242,6 +229,24 @@ def plot_novelty_detection(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac
     label_diseased = "Leukemic"
     label_x = "LAIP1"
     label_y = "LAIP2"
+
+    def plot_decision_boundary(control_data, xlims, ylims, ax, k=5):
+        # To-do: refractor
+        label_healthy = "Non-leukemic"
+        label_diseased = "Leukemic"
+        label_x = "LAIP1"
+        label_y = "LAIP2"
+
+        # Create a mesh to plot in
+        xx, yy = np.meshgrid(np.arange(xlims[0], xlims[1], 0.1), np.arange(ylims[0], ylims[1], 0.1))
+        grid_points = np.c_[xx.ravel(), yy.ravel()]
+        kdtree = cKDTree(control_data[["LAIP1", "LAIP2"]])
+        distances, indices = kdtree.query(grid_points, k=k)
+        dist = np.median(distances, axis=1)
+        Z = np.where(dist > 0.5, 1, 0)
+        Z = Z.reshape(xx.shape)
+        cmap = LinearSegmentedColormap.from_list("custom_cmap", ["#009292ff", "#ff7f00ff"])
+        ax.contourf(xx, yy, Z, cmap=cmap, alpha=0.2)
     
     def plot_scatter(data, title, ax):
         # To-do: refractor
@@ -249,51 +254,45 @@ def plot_novelty_detection(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac
         label_diseased = "Leukemic"
         label_x = "LAIP1"
         label_y = "LAIP2"
-        sns.scatterplot(data=data, x="LAIP1", y="LAIP2", hue="Prediction", palette="tab10", legend=None, ax=ax)        
+
+        sns.scatterplot(data=data, x=label_x, y=label_y, s=4, hue="Cluster", hue_order=[label_healthy, label_diseased],
+        palette=["#009292ff", "#ff7f00ff"], ax=ax, rasterized=True, legend=None)          
         ax.set_xlim(xlims)
         ax.set_ylim(ylims)
         ax.set_title(title)
-
-    def plot_roc_curve(ax, data, label):
-        # To-do: refractor
-        label_healthy = "Non-leukemic"
-        label_diseased = "Leukemic"
-        label_x = "LAIP1"
-        label_y = "LAIP2"
-        fpr, tpr, _ = roc_curve(np.where(data["Cluster"] == label_diseased, 1, 0), data["Enrichment"])
-        roc_auc = auc(fpr, tpr)
-        ax.plot(fpr, tpr, label=f'{label} (AUC = {roc_auc:.2f})')
 
     NBM, AML1, AML2, AML3 = simulate_samples(n_cells, frac_leukemic_AML1, frac_leukemic_AML2, frac_leukemic_AML3,
                                              mean_healthy_x, std_healthy_x, mean_healthy_y, std_healthy_y,
                                              dist_healthy_LAIP1, std_LAIP1_x, std_LAIP1_y,
                                              dist_healthy_LAIP2, std_LAIP2_x, std_LAIP2_y)
 
+    xlims = (-3, 4)
+    ylims = (-3, 4)
 
-    xlims = (-6, 6)
-    ylims = (-6, 6)
-    
     # Get cluster-with-normal results
     AML1["dist"] = novelty_detection(NBM, AML1)
     AML2["dist"] = novelty_detection(NBM, AML2)
     AML3["dist"] = novelty_detection(NBM, AML3)
 
-    AML1["Prediction"] = np.where(AML1["dist"] > 0.1, label_diseased, label_healthy)
-    AML2["Prediction"] = np.where(AML2["dist"] > 0.1, label_diseased, label_healthy)
-    AML3["Prediction"] = np.where(AML3["dist"] > 0.1, label_diseased, label_healthy)
+    AML1["Prediction"] = np.where(AML1["dist"] > 0.5, label_diseased, label_healthy)
+    AML2["Prediction"] = np.where(AML2["dist"] > 0.5, label_diseased, label_healthy)
+    AML3["Prediction"] = np.where(AML3["dist"] > 0.5, label_diseased, label_healthy)
 
-    fig = plt.figure(figsize=(20, 5))
+    fig = plt.figure(figsize=(5.25, 2))
     gs = gridspec.GridSpec(1, 3, figure=fig)
 
     ax1 = fig.add_subplot(gs[0, 0])
+    plot_decision_boundary(NBM, xlims, ylims, ax1, k=5)
     plot_scatter(AML1, "AML1", ax1)
 
     ax2 = fig.add_subplot(gs[0, 1])
+    plot_decision_boundary(NBM, xlims, ylims, ax2, k=5)
     plot_scatter(AML2, "AML2", ax2)
 
     ax3 = fig.add_subplot(gs[0, 2])
+    plot_decision_boundary(NBM, xlims, ylims, ax3, k=5)
     plot_scatter(AML3, "AML3", ax3)
 
     plt.tight_layout()
-    plt.show()
+    plt.close(fig)
     return fig
